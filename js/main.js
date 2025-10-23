482a23c06320c542bc661c1988ade85722132a4f
206ET <ematad0456@gmail.com>
2025-10-23 16:32:28 -0700

    section numbering and navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".counted");
+    sections.forEach((section, i) => section.textContent = `${i + 1}. ${section.textContent}`);
+});
+
+const navLinks = document.querySelectorAll(".navbar a");
+navLinks.forEach(link => {
+    link.addEventListener("click", function () {
+        document.querySelector(".navbar .active").classList.remove("active");
+        this.classList.add("active");
+    });
+});
 
